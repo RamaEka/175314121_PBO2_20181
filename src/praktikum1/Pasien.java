20a2628e9f55e95ae991b67847259d09b0568bed
RamaEka <RamaEka@DESKTOP-7C4NLUQ>
2018-09-04 20:50:32 +0700

    Praktikum 1

---
+++ src/praktikum1/Pasien.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum1;
+
+/**
+ *
+ * @author RamaEka
+ */
+public class Pasien {
+    private String noRekamMedis,nama,alamat,tempatLahir;
+    private int tanggalLahir,bulanLahir,tahunLahir;
+    
+      
+    public Pasien(){
+        
+    }
+    
+
+    public String getNoRekamMedis() {
+        return noRekamMedis;
+    }
+
+    public void setNoRekamMedis(String noRekamMedis) throws NumberFormatException {
+        if (noRekamMedis.toCharArray().length>=6) {
+            this.noRekamMedis = noRekamMedis;
+        }else{
+                throw new NumberFormatException("Nomor Rekam Medis Salah");
+                }
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getTempatLahir() {
+        return tempatLahir;
+    }
+
+    public void setTempatLahir(String tempatLahir) {
+        this.tempatLahir = tempatLahir;
+    }
+
+    public int getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(int tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public int getBulanLahir() {
+        return bulanLahir;
+    }
+
+    public void setBulanLahir(int bulanLahir) {
+        this.bulanLahir = bulanLahir;
+    }
+
+    public int getTahunLahir() {
+        return tahunLahir;
+    }
+
+    public void setTahunLahir(int tahunLahir) {
+        this.tahunLahir = tahunLahir;
+    }
+    
+}
