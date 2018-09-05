3cddf6df6744d832d8f4737c1c1629cd4bdb7f39
admin <admin@7b>
2018-09-05 12:05:11 +0700

    Versi 0.5

---
+++ src/View/Test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+/**
+ *
+ * @author admin
+ */
+public class Test {
+    public static void main(String[] args) {
+        DaftarAntrianDialog test = new DaftarAntrianDialog();
+        test.setSize(600, 400);
+        test.setVisible(true);
+    }
+}
