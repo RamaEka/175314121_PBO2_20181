fa1f2ea0babd20e560a929b2a159835da8186bb1
admin <admin@7b>
2018-09-12 12:04:35 +0700

    versi 0.8

---
+++ src/Test/TestTambahPasienBaruDialog.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import View.DaftarAntrianDialog;
+
+/**
+ *
+ * @author admin
+ */
+public class TestTambahPasienBaruDialog {
+    public static void main(String[] args) {
+        DaftarAntrianDialog test = new DaftarAntrianDialog();
+        test.setSize(600, 400);
+        test.setVisible(true);
+    }
+}
