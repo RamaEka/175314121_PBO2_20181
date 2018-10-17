4a91fb590c9c21d544f93313ccba5ba1952c3773
admin <admin@7b>
2018-10-17 12:28:36 +0700

    17Oktober2018

--- src/Test/TestStream2.java
+++ src/Test/TestStream2.java
@@ -15,8 +15,8 @@ import java.io.File;
 public class TestStream2 {
     public static void main(String[] args) {
         Pasien.bacaDaftarPasien(new File("daftar.txt"));
-        for (int i = 0; i < Pasien.getDaftarPasien().size; i++) {
-            System.out.println(Pasien.getDaftarPasien().get(i).toString());
+        for (int i = 0; i < Pasien.daftarPasienKlinik.size(); i++) {
+            System.out.println(Pasien.daftarPasienKlinik.get(i).toString());
            
         }
     }
